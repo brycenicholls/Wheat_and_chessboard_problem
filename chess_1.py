bd5930b51d82ed94fcd034dc2a3a989c01a0714a
bryce <brycenicholls@gmail.com>
2018-09-01 09:33:06 +0100

    Start of second version

--- chess_1.py
+++ chess_1.py
@@ -1,3 +1,41 @@
 #!/usr/bin/env python3
 
 # second version
+
+# define functions below
+
+
+def add(x, y):
+    """add"""
+    return x + y
+
+
+def sub(x, y):
+    """subtract"""
+    return x - y
+
+
+def div(x, y):
+    """divide"""
+    return x / y
+
+
+def mult(x, y):
+    """multiply"""
+    return x * y
+
+
+def mod(x, y):
+    """remainder"""
+    return x % y
+
+
+def power(x, y):
+    """exponentiation"""
+    return x ** y
+
+# assign variables
+
+
+TILE = 1
+GRAIN = 1
